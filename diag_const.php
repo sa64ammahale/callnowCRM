e4a5a738a2d753c1f320ef033efa693bae5c4df3
sa64ammahale <sa64ammahale@gmail.com>
2026-07-19 18:48:45 +0530

    Fixed d small bugs

--- diag_const.php
+++
@@ -1,7 +0,0 @@
-<?php
-header('Content-Type: text/plain; charset=utf-8');
-require_once "config.php";
-foreach (['TBL_MAIN','TBL_USERS','TBL_LEADS','TBL_APP_SETTINGS','TBL_ROLE_PERMISSIONS','TBL_USER_PERMISSIONS','TBL_TEAMS'] as $c) {
-    echo "$c = " . (defined($c) ? var_export(constant($c), true) : 'UNDEFINED') . "\n";
-}
-echo "DONE\n";
